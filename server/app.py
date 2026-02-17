26575631c487470399f2ff302e0da01c6d5d9371
JeffM20501 <kimmuna440@gmail.com>
2026-02-17 16:33:09 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -22,6 +22,27 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake=Earthquake.query.filter(Earthquake.id==id).first()
+    if earthquake:
+        body=earthquake.to_dict()
+        status=200
+    else:
+        body={'message': f'Earthquake {id} not found.'}
+        status=404
+    response=make_response(body, status)
+    return response
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes=[]
+    for quake in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+        quakes.append(quake.to_dict())
+    body={'count':len(quakes),
+        'quakes':quakes}
+    status=200
+    return make_response(body,status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
